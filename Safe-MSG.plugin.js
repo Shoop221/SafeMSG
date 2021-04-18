4934004ff4948d6c98b1b8e645975394fe422308
Shoop221 <75148039+Shoop221@users.noreply.github.com>
2021-04-17 21:12:53 -0400

    Update Safe-MSG.plugin.js

--- Safe-MSG.plugin.js
+++ Safe-MSG.plugin.js
@@ -1,5 +1,5 @@
 /**
- * @name Safe-MSG
+ * @name SafeMSG
  * @authorId 342035782204129285
  * @authorLink https://github.com/Shoop221
  * @source https://github.com/Shoop221/Safe-MSG/blob/main/Safe-MSG.plugin.js
@@ -8,7 +8,7 @@
     const version = "1";
     const config = {
         info: {
-            name: "Safe-MSG",
+            name: "SafeMSG",
             authors: [{
                 name: "Shoop221",
                 discord_id: "6342035782204129285",
@@ -26,3 +26,49 @@
                 "Fixed / added some stuff",
             ]
         }, ],
+    }
+        return (([Plugin, Api]) => {
+
+            const plugin = (Plugin, Api) => {
+                const {
+                    DiscordModules,
+                    Patcher
+                } = Api;
+                return class SafeMSG extends Plugin {
+                    constructor() {
+                        super();
+                    }
+    
+                    onStart() {
+                        var CryptoJS = require("crypto-js");
+                        Patcher.after(DiscordModules.MessageActions, "sendMessage", (_, [, message]) => {
+                            const content = message.content.toLowerCase();
+    
+    
+                            // Commands
+                            switch (content.split("$")[0]) {
+                                case "encry":
+                                    var password = "test"
+                                    message.content = message.content.substr("$encry", CryptoJS.AES.encrypt(message.content, password).toString());
+                                    break; 
+                            }
+                        });
+                    }
+    
+                    getSettingsPanel() {
+                        
+                    }
+    
+                    onStop() {
+                        Patcher.unpatchAll();
+                    }
+                }
+            };
+    
+            return plugin(Plugin, Api);
+        })(global.ZeresPluginLibrary.buildPlugin(config));
+    })();
+ 
+
+
+
