dee20ada873cce7a6b0bceed1cb0198cd9d523c0
Shoop221 <75148039+Shoop221@users.noreply.github.com>
2022-02-01 21:33:45 -0500

    Update SafeMSG.plugin.js

--- SafeMSG.plugin.js
+++ SafeMSG.plugin.js
@@ -8,6 +8,7 @@
  var password;
  module.exports = (() => {
   const version = "1.8";
+  const name = "SafeMSG";
   const config = {
     info: {
       name: "SafeMSG",
@@ -71,17 +72,7 @@
 
 
         onStart() {
-          if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
-        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "LINK_TO_RAW_CODE");
-
-
-        if (window.ZeresPluginLibrary) {
-          global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(
-            "SafeMSG",
-            version,
-            "https://raw.githubusercontent.com/Shoop221/SafeMSG/main/SafeMSG.plugin.js"
-          );
-        }
+          ZLibrary.PluginUpdater.checkForUpdate(name, version, "https://raw.githubusercontent.com/Shoop221/SafeMSG/main/SafeMSG.plugin.js");
 
         patcher();
 
